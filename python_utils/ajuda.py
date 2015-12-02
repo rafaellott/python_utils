6cda668585fba0dd4fc8a45d26588d5e8062b5c4
Rafael Lott <rafael.lott2@gmail.com>
2015-12-02 14:03:08 -0200

    Add docstring to file ajuda.py

--- python_utils/ajuda.py
+++ python_utils/ajuda.py
@@ -1,3 +1,4 @@
+u"""Arquivo responsável por criar o manual de uso do script."""
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------
 # @name:        ajuda.py
@@ -8,6 +9,7 @@
 
 
 def to_unicode(s, encodings=['utf-8', 'latin-1']):
+    """Try to decode string to Unicode format."""
     if isinstance(s, (list, tuple)):
         return [to_unicode(i) for i in s]
     if isinstance(s, dict):
@@ -25,12 +27,14 @@ def to_unicode(s, encodings=['utf-8', 'latin-1']):
 
 
 def to_encode(s, encoding='utf-8', errors='strict'):
-    # Encodes "DEEP" S using the codec registered for encoding. encoding
-    # defaults to the default encoding. errors may be given to set a different
-    # error handling scheme. Default is 'strict' meaning that encoding errors
-    # raise a UnicodeEncodeError. Other possible values are 'ignore', 'replace'
-    # and 'xmlcharrefreplace' as well as any other name registered with
-    # codecs.register_error that can handle UnicodeEncodeErrors.
+    """Encode "DEEP" S using the codec registered for encoding.
+
+    Errors may be given to set a different error handling scheme.  Default
+    is 'strict' meaning that encoding errors raise a UnicodeEncodeError. Other
+    possible values are 'ignore', 'replace' and 'xmlcharrefreplace'
+    as well as any other name registered with codecs. Register_error that can
+    handle UnicodeEncodeErrors.
+    """
     s = to_unicode(s)
     if isinstance(s, unicode):
         return s.encode(encoding, errors)
@@ -47,8 +51,18 @@ def to_encode(s, encoding='utf-8', errors='strict'):
 
 
 class Ajuda(object):
-    """docstring for """
+    """Class responsible to create the manual page for an script."""
+
     def __init__(self):
+        """Constructor Class.
+
+        Keyword arguments:
+        name        => Section NAME (Contains the script name)
+        syntax      => Section SYNTAX (Show how to script syntax)
+        description => Section DESCRIPTION (More detail for the script)
+        arguments   => Section ARGUMENTS (Sections with the script arguments)
+        others      => Section OTHERS (If you need to create more sections)
+        """
         self.name = ''
         self.syntax = ''
         self.description = ''
@@ -56,17 +70,36 @@ class Ajuda(object):
         self.others = ''
 
     def add_section_name(self, text):
+        """Add the string for the section NAME (In portuguese).
+
+        Keyword arguments:
+        text -- the string that will be shown (string - required)
+        """
         self.add_section("NOME", text)
 
     def add_section_syntax(self, text):
+        """Add the string for the section SYNTAX (In portuguese).
+
+        Keyword arguments:
+        text -- the string that will be shown (string - required)
+        """
         self.add_section("SINTAXE", text)
 
     def add_section_description(self, text):
+        """Add the string for the section DESCRIOPTION (In portuguese).
+
+        Keyword arguments:
+        text -- the string that will be shown (string - required)
+        """
         self.add_section("DESCRIÇÃO", text)
 
     def add_section(self, name, text):
-        # bold SECTION_NAME end_bold \n
-        #   \tTEXT\n\n
+        """Add a new section based on the parameter given.
+
+        Keyword arguments:
+        name -- section title (string - required)
+        text -- section description that will be shown (string - required)
+        """
         name = to_encode(name.upper())
         text = to_encode(text)
         if name == "NOME":
@@ -88,11 +121,22 @@ class Ajuda(object):
         self, name, description, type_arg, opt=None, default=None,
         mandatory=False
     ):
+        """Add a each parameter in section ARGUMENTS.
+
+        Keyword arguments:
+        name -- parameter name (string - required)
+        descriptiion -- description for the parameter (string - required)
+        type_arg -- type of the parameter (string - required)
+        opt -- possible options for parameter (string - default:None)
+        default -- dafault value for parameter (string - default:None)
+        mandatory -- if parameter is required (boolean - default: None)
+        """
         name = to_encode(name)
         description = to_encode(description)
         type_arg = to_encode(type_arg)
-        opt = to_encode((u'[' + opt + ']')) if opt else ''
-        default = to_encode((u'[' + default + ']')) if default else ''
+        mandatory = to_encode(u'OBRIGATÓRIO') if mandatory else 'none'
+        opt = to_encode(opt) if opt else 'none'
+        default = to_encode(default) if default else 'none'
 
         if not self.arguments:
             self.arguments = self.color("PARÂMETROS", "BOLD") + '\n\t'
@@ -104,14 +148,22 @@ class Ajuda(object):
                 )
             )
 
-        mandatory = to_encode((u'[OBRIGATÓRIO]')) if mandatory else ''
-
         self.arguments += (
-            '\t' + self.color(name, 'BOLD') + " [" + type_arg + "]" + opt +
-            default + mandatory + '\n' + '\t\t' + description + '\n\n'
+            '\t' + self.color(name, 'BOLD') + ' [' + type_arg + ']' +
+            '[' + opt + '][' + default + '][' + mandatory + ']\n' +
+            '\t\t' + description + '\n\n'
         )
 
     def color(self, text, *colors):
+        """Print message with color, bold or underline.
+
+        Keyword arguments:
+        text -- text that will be customize (string - required)
+        colors -- color name to be printed (array|string - required)
+
+        Colors options:
+        PURPLE, CYAN, DARKCYAN, BLUE, GREEN, YELLOW, RED, BOLD, UNDERLINE
+        """
         COLOR = {
             'PURPLE': '\033[95m',
             'CYAN': '\033[96m',
@@ -126,12 +178,22 @@ class Ajuda(object):
         }
         resp = ''
         for cor in colors:
-            resp += COLOR[cor]
+            resp += COLOR[cor.upper()]
         resp += text
         resp += COLOR['END']
         return resp
 
     def to_string(self):
+        """Add a each parameter in section ARGUMENTS.
+
+        Keyword arguments:
+        name -- parameter name (string - required)
+        descriptiion -- description for the parameter (string - required)
+        type_arg -- type of the parameter (string - required)
+        opt -- possible options for parameter (string - default:None)
+        default -- dafault value for parameter (string - default:None)
+        mandatory -- if parameter is required (boolean - default: None)
+        """
         if not (self.name and self.syntax and self.description):
             print("[ERROR] Seções obrigatórias não foram preenchidas!")
         else:
