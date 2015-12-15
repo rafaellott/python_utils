70c53cb0fe78206f3b8896d7a67f197ddc86e7a4
rafaellott <rafael.lott2@gmail.com>
2015-12-15 20:03:56 -0200

    Improving unittest

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 
 # from python_utils.helper import _Helper
+#from python_utils import helper
+
 import unittest
-from python_utils import helper
+import python_utils
+
+class TestPprint(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_01_pprint(self):
+        print((python_utils.helper))
+
+    def test_02_pprint(self):
+        print((python_utils._Helper))
+
