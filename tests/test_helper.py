52d8442068af3fbd848c32334327e48e623769c2
rafaellott <rafael.lott2@gmail.com>
2015-12-18 16:23:33 -0200

    Change test class name

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
+"""Tests for _Helper class."""
 
 # from python_utils.helper import _Helper
-#from python_utils import helper
+# from python_utils import helper
 
 import unittest
-import python_utils
+from python_utils.helper import _Helper
 
-class TestPprint(unittest.TestCase):
+
+class TestPythonUtils(unittest.TestCase):
+    """Add documentation here."""
 
     def setUp(self):
-        pass
+        """Add documentation here."""
+        self.helper = _Helper()
 
     def tearDown(self):
-        pass
+        """Add documentation here."""
+        del self.helper
 
+    def test__1_add_section(self):
+        """Add documentation here."""
+        self.helper.add_section_name("TESTE")
+        print self.helper.name
