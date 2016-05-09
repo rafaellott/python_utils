33db01a3ce226a52b3a933549a8099b1a6d39d1a
Rafael Lott <rafaellott@users.noreply.github.com>
2016-05-09 17:56:37 -0300

    Creation of timed rotating file handler example

---
+++ python_utils/log_example.py
@@ -0,0 +1,20 @@
+from logging.handlers import TimedRotatingFileHandler
+import logging
+
+
+def create_timed_rotating_log(path_log, when="midnight", last=3, interval=1):
+    """Create a log based on TimedRotatingFileHandler."""
+    logger = logging.getLogger("Test rotating file")
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+    if path_log:
+        hdlr = TimedRotatingFileHandler(
+            path_log, when=when, interval=interval, backupCount=last
+        )
+        hdlr.setFormatter(formatter)
+        logger.addHandler(hdlr)
+        logger.debug("[LOG] Criou arquivo de log")
+    else:
+        hdlr2 = logging.StreamHandler()
+        hdlr2.setFormatter(formatter)
+        logger.addHandler(hdlr2)
+    return logger
